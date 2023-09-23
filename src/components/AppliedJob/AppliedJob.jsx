672b13b0698fc915d334ac54ff88c4d3d5eac3ef
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-22 22:04:31 -0700

    added sort by job type

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+
+import PropTypes from "prop-types"
 
 const AppliedJob = ({ appliedJob }) => {
-    const {logo} = appliedJob;
+    const {logo,job_title,remote_or_onsite,job_description} = appliedJob;
     return (
         <div className="card card-compact  bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <h2 className="card-title">{job_title}</h2>
+                <p>{job_description}</p>
+                <p>Job Type: {remote_or_onsite}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">View Details</button>
                 </div>
             </div>
         </div>
     );
 };
-
+AppliedJob.propTypes = {
+    appliedJob: PropTypes.object
+}
 export default AppliedJob;
--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -8,28 +8,53 @@ const AppliedJobs = () => {
     const jobs = useLoaderData();
 
     const [appliedJobs, setAppliedJobs] = useState([])
+    const [displayJobs, setDisplayJobs] = useState([])
+
+    const handleJobByFilter = filter => {
+        if(filter === 'all'){
+            setDisplayJobs(appliedJobs)
+        }else if( filter === 'remote'){
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs)
+        }
+        else if ( filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite')
+            setDisplayJobs(onsiteJobs)
+        }
+    }
     useEffect(() => {
         const storedJobIds = getStoredApplication()
-        
-        if(jobs.length > 0){
+
+        if (jobs.length > 0) {
             // const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
             // console.log(jobsApplied);
             const jobsApplied = [];
-            for(const id of storedJobIds){
+            for (const id of storedJobIds) {
                 const job = jobs.find(job => job.id === id)
-                if(job){
+                if (job) {
                     jobsApplied.push(job)
                 }
             }
             setAppliedJobs(jobsApplied)
+            setDisplayJobs(jobsApplied)
         }
-    },[])
-
+    }, [])
+console.log(displayJobs);
     return (
         <div>
             <h2>Applied Jobs: {appliedJobs.length}</h2>
+            <div>
+                <details className="dropdown mb-32">
+                    <summary className="m-1 btn">open or close</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li onClick={() => handleJobByFilter('all')}><a>All</a></li>
+                        <li onClick={() => handleJobByFilter('remote')}><a>Remote</a></li>
+                        <li onClick={() => handleJobByFilter('onsite')}><a>Onsite</a></li>
+                    </ul>
+                </details>
+            </div>
             {
-                appliedJobs.map(appliedJob => <AppliedJob key={appliedJob.id} appliedJob={appliedJob}></AppliedJob> )
+                displayJobs.map(appliedJob => <AppliedJob key={appliedJob.id} appliedJob={appliedJob}></AppliedJob>)
             }
         </div>
     );
